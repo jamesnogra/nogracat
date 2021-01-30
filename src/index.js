a9fff56fdc32aa63ec2cef1362e7fc725d6c249d
James Arnold <james.arnold.nogra@fullspeedtechnologies.com>
2021-01-30 12:48:12 +0800

    Changed the default component from App to CatComponent.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CatComponent from './CatComponent';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// Bootstrap styling
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+ReactDOM.render(<CatComponent />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
